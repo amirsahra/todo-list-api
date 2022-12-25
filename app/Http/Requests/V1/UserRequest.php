2f4b1147136f14125286fd7f6f0fca1477c7b7d3
amirsahra <amirhosein.sahra@gmail.com>
2022-12-25 19:02:05 +0330

    feat: validation and access

---
+++ app/Http/Requests/V1/UserRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (auth('api')->id() == $this->user->id)
+            return true;
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => ['min:3', 'max:225'],
+            'last_name' => ['min:3', 'max:225'],
+            'birthday' => ['date'],
+            'gender' => [Rule::in(['female', 'male'])],
+            'phone' => ['regex:/^([0-9\s\-\+\(\)]*)$/', 'min:10', Rule::unique('users')],
+            'avatar' => [Rule::imageFile()], //'mimes:jpeg,jpg,png,gif|max:20000'
+            'password' => ['required', 'min:6']
+        ];
+    }
+}
