41441d790864d6e2a91824294d12acaf1a492a18
amirsahra <amirhosein.sahra@gmail.com>
2022-12-25 18:22:38 +0330

    feat: admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth('api')->type == 'admin')
+            return $next($request);
+
+        return response()->apiResult(__('messages.validate_authorized'),
+            ['authorization' => __('messages.unauthorized')],
+            false,
+            403
+        );
+    }
+}
