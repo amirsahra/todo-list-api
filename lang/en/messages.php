0a3259d1241339137c1a1d8bba5ac87e265370b5
amirsahra <amirhosein.sahra@gmail.com>
2022-12-26 11:49:38 +0330

    feat: create execution blade

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -45,6 +45,11 @@ return [
             'content' => "You are receiving this email because we received a password reset request for your account." .
                 "\nThis password reset link will expire in 60 minutes." .
                 "\nIf you did not request a password reset, no further action is required",
+        ],'
+        execution' => [
+            'subject' => 'Execution time',
+            'title' => 'Task time reminder',
+            'content' => "This email is only to remind you of your task.",
         ],
     ],
 
