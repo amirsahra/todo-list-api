3c102839ab90b7608b4a43daccdea5d66432b3e5
amirsahra <amirhosein.sahra@gmail.com>
2022-12-26 11:48:52 +0330

    feat: create send mail job

---
+++ app/Jobs/SendMailJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendMailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $email;
+    protected $emailClass;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $emailClass)
+    {
+        $this->email = $email;
+        $this->emailClass= $emailClass;
+    }
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->email)->send($this->emailClass);
+    }
+}
